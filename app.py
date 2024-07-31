87af40342389f1b4f9c8af892e2dd06e7f2e881c
Amir Shahcheraghian <amir.shahcheraghian@gmail.com>
2024-07-31 09:50:16 -0400

    update

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
