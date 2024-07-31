dd1b1bee7aa9ca0e4d89c8f09fc5548e7de16426
Amir Shahcheraghian <amir.shahcheraghian@gmail.com>
2024-07-31 09:49:25 -0400

    updtae

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import pandas as pd
 import numpy as np 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
