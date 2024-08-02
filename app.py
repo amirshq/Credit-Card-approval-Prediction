bc606dc9cabc9912ec10e524da9b05d7c745aeed
Amir Shahcheraghian <amir.shahcheraghian@gmail.com>
2024-08-02 12:00:45 -0400

    update data

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+application_record_df = pd.read_csv('/Users/amirshahcheraghian/Desktop/Desktop/PHD ETS/Python Codes/Kaggle /Credit Card Approval Prediction/application_record.csv')
+credit_card_record_df = pd.read_csv('/Users/amirshahcheraghian/Desktop/Desktop/PHD ETS/Python Codes/Kaggle /Credit Card Approval Prediction/credit_record.csv')
